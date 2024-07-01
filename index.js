39f1cc4583e105338ed0449ccb49e630abb48e4f
Nathan Mwai <nathan.mwai@student.moringaschool.com>
2024-07-01 08:18:44 +0300

    Updates js

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = divide1 => {
+    let a = 2000
+    let b = 100
+    return a / b
+}
+
+const square = square1 => Math.pow(2, 2)
+// pow squares a number ie _math.pow_
+
+const add = addition => 3 + 4
